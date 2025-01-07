934548ab571f730a3c3538c864b0fa58237ec4ef
iamriatandan <tandanria@gmail.com>
2025-01-07 14:00:07 +0530

    new updation

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,8 @@
 class Welcome{
 	public static void main(Strong[]args){
 System.out.println("Hello java");
+System.out.println(addition(2,4);
 }
+public static int addition(int a,int b ){
+	return a+b;
 }
